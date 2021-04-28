634fa9cf778b74f260c68b1155ce08747e04085d
Amir_progg <75519329+Amir02042009@users.noreply.github.com>
2021-04-28 12:45:39 +0300

    Update the first 100 characters from the book.py

--- the first 100 characters from the book.py
+++ the first 100 characters from the book.py
@@ -1,7 +1,7 @@
 #we translate a .txt file or book in .txt format into the same folder where and python file
 def split_into_sentences():
   a = int(input())#enter the number of characters that we want to be removed from the book
-  handle = open('The Burning Secret.txt')#Choosing a book and writing it in quotes ('name.txt')
+  handle = open('MyBook.txt')#Choosing a book and writing it in quotes ('name.txt')
   data = handle.read(a)#wrine number
   print(data)#print these characters from the book
   handle.close()#close file
