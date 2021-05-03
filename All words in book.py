e22d40131e0b26853f54a41d7be4962d3fdb25aa
Amir_progg <75519329+Amir02042009@users.noreply.github.com>
2021-05-03 20:27:16 +0300

    Update All words in book.py

--- All words in book.py
+++ All words in book.py
@@ -1,6 +1,6 @@
 #Download regex with help comand "pip install regex"
 #Write all words in book
-import regex
+import re
 #we translate a .txt file or book in .txt format into the same folder where and python file
 def split_into_sentences():
   handle = open('The Burning Secret.txt')#Choosing a book and writing it in quotes ('name.txt')
